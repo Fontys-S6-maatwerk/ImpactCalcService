f44b05832433446dd1ea4c3961c600e398d087bd
Tim Gooren <384376@student.fontys.nl>
2021-10-07 13:54:26 +0200

    Add barebones project

---
+++ ImpactCalcService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImpactCalcService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
